fcb1b2fcdbfc8313ea90994ca28b37c552c18b2b
totoro.fu <1132405295@qq.com>
2025-03-17 17:58:06 +0800

    手动安装

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ function App() {
   useEffect(() => {
     const loadSerwist = async () => {
       if ("serviceWorker" in navigator) {
-        alert("serviceWorker");
         const serwist = await getSerwist();
         serwist?.addEventListener("installed", () => {
           console.log("Serwist installed!");
@@ -23,10 +22,20 @@ function App() {
     loadSerwist();
   });
 
+  useEffect(() => {
+    const installBtn = document.getElementById("installBtn");
+    installBtn.addEventListener("click", () => {
+      const pwaInstall = document.getElementsByTagName("pwa-install")[0];
+      console.log("pwaInstall", pwaInstall);
+      pwaInstall.showDialog();
+    });
+  }, []);
+
   return (
     <>
       <div>
-        <pwa-install></pwa-install>
+        <pwa-install manual-apple="true" manual-chrome="true"></pwa-install>
+        <button id="installBtn">安装 PWA</button>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
